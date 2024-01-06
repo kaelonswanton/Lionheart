83a42407afd90ad0dbac205b76fff3f7c4b276ee
kaelonswanton <kaelonswanton@gmail.com>
2024-01-05 19:00:54 -0800

    model updates

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,7 +4,6 @@ class Product < ApplicationRecord
   end
   has_many :category_product_links, dependent: :destroy
   has_many :categories, through: :category_product_links
-  has_many :line_items, dependent: :destroy
   has_one_attached :image
   #allows to create a category from the product form
   #accepts_nested_attributes_for :categories, reject_if: :all_blank
